c5fe4468d7e531e384585dc7a3d184cd9a41f0d5
unknown <151481930+Sebas-lop@users.noreply.github.com>
2024-09-07 18:53:13 -0500

    correccion de dinero

--- semana-1/billetes.cpp
+++ semana-1/billetes.cpp
@@ -4,23 +4,27 @@ using namespace std;
 
 void cambio(double money){
     int aux =0;       
-    float pRelat=money;
-    float lista[]={200,100,50,20,10,5,2,1,0.50,0.20,0.10};
+    int pRelat= static_cast<int>(money*100);
+    int lista[]={20000,10000,5000,2000,1000,500,200,100,50,20,10};
     cout<<"**********CAMBIO**********"<<endl;
     for(int i=0;i<11;i++){
         aux=pRelat/lista[i];               
-        pRelat=pRelat-(lista[i]*aux);
-        cout<<"Billetes de "<<setw(3)<<lista[i]<<" Soles = "<<aux<<endl;
+        pRelat=pRelat-(lista[i]*aux);      
+        
+        cout<<"Billetes de "<<setw(3)<<lista[i]/100.00<<" Soles = "<<aux<<endl;
+
+        
+        
     }
-    
-}
+}    
+
 
 
 
 
 
 int main(){
-    cambio(2135.80);
+    cambio(3545.70);
 
     return 0;
 }
