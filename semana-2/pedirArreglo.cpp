7a96e27f7c88791c43c240e4c15ce08ccb105f06
Sebastián López <164351009+SebasLopDev@users.noreply.github.com>
2024-09-19 16:45:05 -0500

    Create pedirArreglo.cpp

---
+++ semana-2/pedirArreglo.cpp
@@ -0,0 +1,10 @@
+ int tam;
+    cout << "Introduzca el tamaño del arreglo" << endl;
+    cin >> tam;
+    int* arreglo2=new int[tam];     
+    int* ptr = arreglo2;
+    for (int* i = arreglo2; i < arreglo2+tam;i++) {
+        cin >> *i;
+        
+    }
+    cocktail(arreglo2, arreglo2 +tam-1);
